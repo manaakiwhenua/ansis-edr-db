1e1618e6f11c233e42f2fee4380fdc5e9fe211fa
Alistair Ritchie <ritchiea@landcareresearch.co.nz>
2024-10-24 10:56:27 +1300

    Added working table to create content for cm.class.

---
+++ data/class.sql
@@ -0,0 +1,52 @@
+-- drop view if exists working.class;
+create or replace view working.class as
+select
+    defs.obj ->> '@id' as id,
+    '3908e757-e482-4412-8759-ec6ef969482c' as class_model_id,
+    defs.key as identifier,
+    false as abstract,
+    defs.obj ->> 'title' as label,
+    defs.obj ->> 'description' as definition,
+    defs.obj ->> '$comment' as editorial_note,
+    null as rdf_match,
+    null as see_also,
+    null as root_class,
+    'edr-entity' as system__type_id,
+    null as system__label_template,
+    null as system__default_location_attribute_id,
+    concat('./', aj.file_name, '#/$defs/', defs.key) as _external_ref,
+    concat('#/$defs/', defs.key) as _internal_ref
+from
+    working.ansis_json aj,
+    jsonb_each(aj.file #> '{$defs}') defs(key,obj)
+where
+    aj.file_name not in ('enum.json', 'geo.json', 'properties.json','properties-chemical.json','properties-physical.json')
+    and defs.obj ->> 'type' = 'object'
+    and jsonb_typeof(defs.obj -> '@id') != 'array'
+union all
+select
+    _id as id,
+    '3908e757-e482-4412-8759-ec6ef969482c' as class_model_id,
+    defs.key as identifier,
+    false as abstract,
+    defs.obj ->> 'title' as label,
+    defs.obj ->> 'description' as definition,
+    defs.obj ->> '$comment' as editorial_note,
+    null as rdf_match,
+    null as see_also,
+    null as root_class,
+    'edr-entity' as system__type_id,
+    null as system__label_template,
+    null as system__default_location_attribute_id,
+    concat('./', aj.file_name, '#/$defs/', defs.key) as _external_ref,
+    concat('#/$defs/', defs.key) as _internal_ref
+from
+    working.ansis_json aj,
+    jsonb_each(aj.file #> '{$defs}') defs(key,obj),
+    jsonb_array_elements_text(defs.obj -> '@id') _id
+where
+    aj.file_name not in ('enum.json', 'geo.json', 'properties.json','properties-chemical.json','properties-physical.json')
+    and defs.obj ->> 'type' = 'object'
+    and jsonb_typeof(defs.obj -> '@id') = 'array';
+alter view working.class owner to edr_wheel;
+grant select on table working.class to edr_admin, edr_jwt, edr_edit, edr_read;
