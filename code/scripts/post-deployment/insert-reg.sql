530ec5c81f77f6baccfecab533b8c74f81e7f93b
Alistair Ritchie <ritchiea@landcareresearch.co.nz>
2024-10-24 10:54:28 +1300

    Added vocabulary register.

--- code/scripts/post-deployment/insert-reg.sql
+++ code/scripts/post-deployment/insert-reg.sql
@@ -9,7 +9,8 @@ truncate table
 -- insert registers
 insert into reg.register (id, default_label, register_id)
     values ('a7187fec-1098-4c4d-a2bf-9d5544deeaa2', 'ANSIS Registry', null),
-           ('54c83d3f-3314-438f-a5b3-54fd34480bdb', 'Domain Model Register', 'a7187fec-1098-4c4d-a2bf-9d5544deeaa2');
+           ('54c83d3f-3314-438f-a5b3-54fd34480bdb', 'Domain Model', 'a7187fec-1098-4c4d-a2bf-9d5544deeaa2'),
+           ('e3f68055-82bf-4e3d-a30c-3bf3eaf4eb1d', 'Vocabularies', 'a7187fec-1098-4c4d-a2bf-9d5544deeaa2');
 
 -- add register__attribute table inserts
 
