df05a2ce5dcbc0f5b04c6fa8a57cb1be41f2a865
Alistair Ritchie <ritchiea@landcareresearch.co.nz>
2024-10-26 12:42:52 +1300

    Added sub-definitions and path-based IDs.

--- data/attribute.sql
+++ data/attribute.sql
@@ -1,6 +1,32 @@
 -- drop view if exists working.attribute cascade;
 create or replace view working.attribute as
 select
+    concat('./', aj.file_name, '#/$defs/', defs.key, '/$defs/' || sub.key) as id,
+    '3908e757-e482-4412-8759-ec6ef969482c'::uuid as class_model_id,
+    defs.key as identifier,
+    null as data_type_id,
+    false as abstract,
+    false as derived,
+    concat(defs.obj ->> 'title', ' - ' || sub.key) as label,
+    case when sub.obj is not null
+         then concat(defs.obj ->> 'description', ' ', sub.obj ->> 'description')
+         else defs.obj ->> 'description'
+    end as definition,
+    defs.obj ->> '$comment' as editorial_note,
+    null::text[] as rdf_match,
+    null::text[] as see_also,
+    null::jsonb as constraints,
+    'edr-assertion' as system__type_id,
+    concat('#/$defs/', defs.key) as _internal_id
+from
+    working.ansis_json aj,
+    jsonb_each(aj.file #> '{$defs}'::text[]) defs(key, obj)
+        left join jsonb_each(defs.obj #> '{$defs}'::text[]) sub(key, obj) on true
+where
+    aj.file_name in ('base.json', 'prov.json', 'qudt.json')
+    and coalesce(defs.obj ->> 'type', 'not an object') != 'object'
+union
+select
     concat('./', aj.file_name, '#/$defs/', defs.key) as id,
     '3908e757-e482-4412-8759-ec6ef969482c'::uuid as class_model_id,
     defs.key as identifier,
@@ -17,20 +43,23 @@ select
     concat('#/$defs/', defs.key) as _internal_id
 from
     working.ansis_json aj,
-    jsonb_each(aj.file #> '{$defs}'::text[]) defs(key, obj)
+    jsonb_each(aj.file #> '{$defs,Project,properties}'::text[]) defs(key, obj)
 where
-    aj.file_name in ('base.json', 'prov.json', 'qudt.json')
-    and defs.obj ->> 'type' != 'object'
+    aj.file_name in ('proj.json')
+    and coalesce(defs.obj ->> '$ref', './base.json#/$defs/pointer') != './base.json#/$defs/pointer'
 union
 select
-    concat('./', aj.file_name, '#/$defs/', defs.key) as id,
+    concat('./', aj.file_name, '#/$defs/', defs.key, '/$defs/' || sub.key) as id,
     '3908e757-e482-4412-8759-ec6ef969482c'::uuid as class_model_id,
     defs.key as identifier,
     null as data_type_id,
     false as abstract,
     false as derived,
-    defs.obj ->> 'title' as label,
-    defs.obj ->> 'description' as definition,
+    concat(defs.obj ->> 'title', ' - ' || sub.key) as label,
+    case when sub.obj is not null
+         then concat(defs.obj ->> 'description', ' ', sub.obj ->> 'description')
+         else defs.obj ->> 'description'
+    end as definition,
     defs.obj ->> '$comment' as editorial_note,
     null::text[] as rdf_match,
     null::text[] as see_also,
@@ -43,18 +72,22 @@ select
 from
     working.ansis_json aj,
     jsonb_each(aj.file #> '{$defs}'::text[]) defs(key, obj)
+        left join jsonb_each(defs.obj #> '{$defs}'::text[]) sub(key, obj) on true
 where
     aj.file_name in ('geo.json')
 union
 select
-    concat('./', aj.file_name, '#/$defs/', defs.key) as id,
+    concat('./', aj.file_name, '#/$defs/', defs.key, '/$defs/' || sub.key) as id,
     '3908e757-e482-4412-8759-ec6ef969482c'::uuid as class_model_id,
     defs.key as identifier,
     null as data_type_id,
     false as abstract,
     false as derived,
-    defs.obj ->> 'title' as label,
-    defs.obj ->> 'description' as definition,
+    concat(defs.obj ->> 'title', ' - ' || sub.key) as label,
+    case when sub.obj is not null
+         then concat(defs.obj ->> 'description', ' ', sub.obj ->> 'description')
+         else defs.obj ->> 'description'
+    end as definition,
     defs.obj ->> '$comment' as editorial_note,
     null::text[] as rdf_match,
     null::text[] as see_also,
@@ -67,19 +100,24 @@ select
 from
     working.ansis_json aj,
     jsonb_each(aj.file #> '{$defs}'::text[]) defs(key, obj)
+        left join jsonb_each(defs.obj #> '{$defs}'::text[]) sub(key, obj) on true
 where
     aj.file_name in ('sosa.json')
-    and defs.key not in ('Sampling', 'sampleOf')
+    and defs.key != 'Sampling'
+    and coalesce(defs.obj ->> '$ref', 'not a pointer') != './base.json#/$defs/pointer'
 union
 select
-    concat('./', aj.file_name, '#/$defs/', defs.key) as id,
+    concat('./', aj.file_name, '#/$defs/_att/$defs/', defs.key, '/$defs/' || sub.key) as id,
     '3908e757-e482-4412-8759-ec6ef969482c'::uuid as class_model_id,
     defs.key as identifier,
     null as data_type_id,
     false as abstract,
     false as derived,
-    defs.obj ->> 'title' as label,
-    defs.obj ->> 'description' as definition,
+    concat(defs.obj ->> 'title', ' - ' || sub.key) as label,
+    case when sub.obj is not null
+         then concat(defs.obj ->> 'description', ' ', sub.obj ->> 'description')
+         else defs.obj ->> 'description'
+    end as definition,
     defs.obj ->> '$comment' as editorial_note,
     null::text[] as rdf_match,
     null::text[] as see_also,
@@ -89,18 +127,22 @@ select
 from
     working.ansis_json aj,
     jsonb_each(aj.file #> '{$defs,_att,$defs}'::text[]) defs(key, obj)
+        left join jsonb_each(defs.obj #> '{$defs}'::text[]) sub(key, obj) on true
 where
     aj.file_name in ('properties.json')
 union
 select
-    concat('./', aj.file_name, '#/$defs/', defs.key) as id,
+    concat('./', aj.file_name, '#/$defs/_obs/$defs/', defs.key, '/$defs/' || sub.key) as id,
     '3908e757-e482-4412-8759-ec6ef969482c'::uuid as class_model_id,
     defs.key as identifier,
     null as data_type_id,
     false as abstract,
     false as derived,
-    defs.obj ->> 'title' as label,
-    defs.obj ->> 'description' as definition,
+    concat(defs.obj ->> 'title', ' - ' || sub.key) as label,
+    case when sub.obj is not null
+         then concat(defs.obj ->> 'description', ' ', sub.obj ->> 'description')
+         else defs.obj ->> 'description'
+    end as definition,
     defs.obj ->> '$comment' as editorial_note,
     null::text[] as rdf_match,
     null::text[] as see_also,
@@ -110,6 +152,7 @@ select
 from
     working.ansis_json aj,
     jsonb_each(aj.file #> '{$defs,_obs,$defs}'::text[]) defs(key, obj)
+        left join jsonb_each(defs.obj #> '{$defs}'::text[]) sub(key, obj) on true
 where
     aj.file_name in ('properties.json')
 union
