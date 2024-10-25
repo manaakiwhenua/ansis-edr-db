dc7d26f5b6bdee2a2a8f833a915f79fd2ab79af8
Alistair Ritchie <ritchiea@landcareresearch.co.nz>
2024-10-26 12:44:32 +1300

    Simplified Class defintions.

--- data/class.sql
+++ data/class.sql
@@ -8,7 +8,10 @@ select
     defs.obj ->> 'title' as label,
     defs.obj ->> 'description' as definition,
     defs.obj ->> '$comment' as editorial_note,
-    array[defs.obj ->> '@id'] as rdf_match,
+    case when jsonb_typeof(defs.obj -> '@id') = 'array'
+         then replace(replace(replace(defs.obj ->> '@id', '[', '{'), ']', '}'), '"', '')::text[]
+         else case when defs.obj ->> '@id' is not null then array[defs.obj ->> '@id'] end
+    end as rdf_match,
     null::text[] as see_also,
     false as root_class,
     'edr-entity' as system__type_id,
@@ -20,31 +23,6 @@ from
     jsonb_each(aj.file #> '{$defs}') defs(key,obj)
 where
     aj.file_name not in ('enum.json', 'geo.json', 'properties.json','properties-chemical.json','properties-physical.json')
-    and defs.obj ->> 'type' = 'object'
-    and jsonb_typeof(defs.obj -> '@id') != 'array'
-union all
-select
-    concat('./', aj.file_name, '#/$defs/', defs.key) as id,
-    '3908e757-e482-4412-8759-ec6ef969482c'::uuid as class_model_id,
-    concat(defs.key, '[', _id, ']') as identifier,
-    false as abstract,
-    concat(defs.obj ->> 'title', ' [', _id, ']') as label,
-    defs.obj ->> 'description' as definition,
-    defs.obj ->> '$comment' as editorial_note,
-    array[defs.obj ->> '@id'] as rdf_match,
-    null::text[] as see_also,
-    false as root_class,
-    'edr-entity' as system__type_id,
-    null as system__label_template,
-    null as system__default_location_attribute_id,
-    concat('#/$defs/', defs.key) as _internal_id
-from
-    working.ansis_json aj,
-    jsonb_each(aj.file #> '{$defs}') defs(key,obj),
-    jsonb_array_elements_text(defs.obj -> '@id') _id
-where
-    aj.file_name not in ('enum.json', 'geo.json', 'properties.json','properties-chemical.json','properties-physical.json')
-    and defs.obj ->> 'type' = 'object'
-    and jsonb_typeof(defs.obj -> '@id') = 'array';
+    and defs.obj ->> 'type' = 'object';
 alter view working.class owner to edr_wheel;
 grant select on table working.class to edr_admin, edr_jwt, edr_edit, edr_read;
