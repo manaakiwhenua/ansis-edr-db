824db28aa4af2eba03aa3cd897fa8ac765044d6f
Alistair Ritchie <ritchiea@landcareresearch.co.nz>
2024-10-26 12:43:23 +1300

    Filtered out pointers and enumeration refs.

--- data/attribute__domain.sql
+++ data/attribute__domain.sql
@@ -2,19 +2,18 @@
 create or replace view working.attribute__domain as
 select
     gen_random_uuid() as id,
-    pd.definition_id as attribute_id,
+    pd.attribute_id,
     pd.class_id,
     pd.constraint__multiplicity,
-    null as constraint__value_collection_id,
-    null as constraint__value_unit_collection_id,
-    null as constraint__procedure_collection_id,
-    null as constraint__agent_collection_id
+    null::uuid as constraint__value_collection_id,
+    null::uuid as constraint__value_unit_collection_id,
+    null::uuid as constraint__procedure_collection_id,
+    null::uuid as constraint__agent_collection_id
 from
     working.property__domain pd
-        left join working.attribute atte on pd.definition_id = atte._external_ref
-        left join working.attribute atti on pd.definition_id = atti._internal_ref
 where
-    not (pd.definition_id ~ '_rel');
+    not (pd.attribute_id ~ '_rel')
+    and pd.attribute_id not in ('./sosa.json#/$defs/usedProcedure/$defs/Enumeration', './base.json#/$defs/pointer');
 alter table working.attribute__domain owner to edr_wheel;
 grant select on table working.attribute__domain to edr_admin, edr_jwt, edr_edit, edr_read;
 
